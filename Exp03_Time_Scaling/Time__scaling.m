ed6bed2e4d29d4e32ec81d89f7fdd928e2dbfc2b
Manish Kumar <manishkumar143255@gmail.com>
2026-01-30 22:41:11 +0530

    Update Time__scaling.m

--- Exp03_Time_Scaling/Time__scaling.m
+++ Exp03_Time_Scaling/Time__scaling.m
@@ -1,8 +1,8 @@
 clc; clear; close all;
 
 % Original signal
-n = -3:3;
-x = [2 -1 3 2 1 3 -2];
+n = -4:4;
+x = [2 -1 3 2 1 -2 3 3 -2];
 
 % 2. Compression 
 % This effectively keeps samples where 2n is integers within range.
@@ -27,29 +27,29 @@ figure
 subplot(5,1,1)
 stem(n, x, 'filled', 'LineWidth', 1)
 title('Original x[n]')
-grid on; axis tight; ylim([-3 4]);
+grid on; axis tight; ylim([-3 4]); xlim([-12 12]);
 xlabel('n'); ylabel('x[n]')
 
 subplot(5,1,2)
 stem(n, x2, 'filled', 'LineWidth', 1)
 title('Time Compressed: x[2n] ')
-grid on; axis tight; ylim([-3 4]);
+grid on; axis tight; ylim([-3 4]); xlim([-12 12]);
 xlabel('n'); ylabel('x[2n]')
 
 subplot(5,1,3)
 stem(n, x3, 'filled', 'LineWidth', 1)
 title('Time Compressed: x[3n]')
-grid on; axis tight; ylim([-3 4]);
+grid on; axis tight; ylim([-3 4]); xlim([-12 12]);
 xlabel('n'); ylabel('x[3n]')
 
 subplot(5,1,4)
 stem(n_exp2, x_n2, 'filled', 'LineWidth', 1)
 title('Time Expanded: x[n/2]')
-grid on; axis tight; ylim([-3 4]);
+grid on; axis tight; ylim([-3 4]); xlim([-12 12]);
 xlabel('n'); ylabel('x[n/2]')
 
 subplot(5,1,5)
 stem(n_exp3, x_n3, 'filled', 'LineWidth', 1)
 title('Time Expanded: x[n/3]')
-grid on; axis tight; ylim([-3 4]);
+grid on; axis tight; ylim([-3 4]); xlim([-12 12]);
 xlabel('n'); ylabel('x[n/3]')
